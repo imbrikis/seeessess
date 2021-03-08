11e14acfab8e3e81b94c60a055d5ca774e091e8c
imbrikis <imbrikis@yahoo.com>
2021-03-07 22:06:27 -0700

    correct click event from firing multiple times

--- scripts/script02.js
+++ scripts/script02.js
@@ -6,16 +6,30 @@ const lineFadeOut = 8
 // const lineColor = '244,25,209,1'
 const lineColor = '29,244,25,1'
 
-const c = document.querySelector('.img')
-c.addEventListener('click', ()=> {
-    console.log('clicked once')
+let intervalId
 
-  const intervalId = setInterval(() => runAnimation(), animationDuration)
-  
-})
+const c = document.querySelector('.img')
+
+if (y1 === 0) {
+  c.addEventListener('click', () => {
+    if (y1 === 0) {
+      intervalId = setInterval(() => runAnimation(), animationDuration)
+    }
+  })
+}
 
 const runAnimation = () => {
-    c.style.backgroundImage = `linear-gradient(rgba(0,0,0,0), 0%, rgba(0,0,0,0), ${y1 === 100 ? '100' : 0 + y1}%, rgba(${lineColor}), ${y1 === 100 ? '100' : lineFadein + y1}%, rgba(0,0,0,0), ${y1 === 100 ? '100' : lineFadeOut + y1}%, rgba(0,0,0,0))`
+  if (y1 > 100) {
+    clearInterval(intervalId)
+    y1 = 0
+    return
+  }
 
-    y1 += animationStep
-} 
+  c.style.backgroundImage = `linear-gradient(rgba(0,0,0,0), 0%, rgba(0,0,0,0), ${
+    y1 === 100 ? '100' : 0 + y1
+  }%, rgba(${lineColor}), ${
+    y1 === 100 ? '100' : lineFadein + y1
+  }%, rgba(0,0,0,0), ${y1 === 100 ? '100' : lineFadeOut + y1}%, rgba(0,0,0,0))`
+
+  y1 += animationStep
+}
