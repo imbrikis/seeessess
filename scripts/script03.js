088031cee0a3ee946ee1ce00217b6a57471cf098
imbrikis <imbrikis@yahoo.com>
2021-03-07 22:52:28 -0700

    setup of page03 script

--- scripts/script03.js
+++ scripts/script03.js
@@ -1,6 +1,19 @@
 // A demonstration of collision detection
 
-const b1 = document.querySelector('.ball-1')
-const b2 = document.querySelector('.ball-2')
+// --- VARIABLES --- //
+const ballA = document.querySelector('.ball-1')
+const ballB = document.querySelector('.ball-2')
+let ballAx = 0
+let ballAy = 0
+let ballBx = 50
+let ballBy = 50
 
-b1.style.top = '10%'
+// initialize the starting points
+ballA.style.top = `${ballAy}%`
+ballA.style.left = `${ballAx}%`
+ballB.style.top = `${ballBy}%`
+ballB.style.left = `${ballBx}%`
+
+// --- ANIMATION STARTS HERE --- //
+ballAy += 10
+ballA.style.top = `${ballAy}%`
