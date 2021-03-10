a6b66b065e5706663c95e6092dd8bd42dbcdb783
imbrikis <imbrikis@yahoo.com>
2021-03-10 09:23:36 -0700

    scroll detection of div within the SVG bounds

--- scripts/script04.js
+++ scripts/script04.js
@@ -1,44 +1,96 @@
 const colorBlock = document.querySelector('.color-block')
 
 const svgPaths = document.getElementsByTagName('path')
-const svgYPositionTop = svgPaths[0].getBoundingClientRect().top
-const svgYPositionBottom =
+const svgYPositionTop = Math.floor(svgPaths[0].getBoundingClientRect().top)
+const svgYPositionBottom = Math.floor(
   svgPaths[0].getBoundingClientRect().height +
-  svgPaths[0].getBoundingClientRect().top
-console.log(svgYPositionTop, svgYPositionBottom)
+    svgPaths[0].getBoundingClientRect().top
+)
+const svgPosition = [svgYPositionTop, svgYPositionBottom]
 
-const div1 = document.querySelector('.one')
-const div2 = document.querySelector('.two')
-const div3 = document.querySelector('.three')
-const div4 = document.querySelector('.four')
+const divs = []
 
-// const compareElements = () => {}
+divs[0] = document.querySelector('.one')
+divs[1] = document.querySelector('.two')
+divs[2] = document.querySelector('.three')
+divs[3] = document.querySelector('.four')
+
+// const init = () => {
+
+//   console.log(svgPaths[0].style)
+// }
+
+// init()
+
+const compareElements = (overlay, underlayItems) => {
+  let elementColors = [svgPaths[0].style.fill || 'rgb(40, 192, 215)']
+  underlayItems.forEach((div, index) => {
+    let underlayYPositionTop = div.getBoundingClientRect().top
+    let underlayYPositionBottom =
+      div.getBoundingClientRect().height + div.getBoundingClientRect().top
+
+    console.log(
+      index,
+      underlayYPositionTop,
+      underlayYPositionBottom,
+      overlay[0],
+      overlay[1]
+    )
+
+    if (
+      underlayYPositionTop <= overlay[0] &&
+      underlayYPositionBottom <= overlay[0]
+    ) {
+      // console.log(`color will not be changed`)
+    } else if (
+      underlayYPositionTop >= overlay[1] &&
+      underlayYPositionBottom >= overlay[1]
+    ) {
+      // console.log(`color will not be changed`)
+    } else if (
+      underlayYPositionTop <= overlay[0] &&
+      underlayYPositionBottom >= overlay[1]
+    ) {
+      console.log(`${div.className} is currently within the SVG`)
+    } else if (
+      underlayYPositionTop >= overlay[0] &&
+      underlayYPositionBottom <= overlay[1]
+    ) {
+      console.log(`${div.className} is currently within the SVG`)
+    } else if (
+      underlayYPositionTop < overlay[1] &&
+      underlayYPositionBottom >= overlay[1]
+    ) {
+      console.log(`${div.className} is currently within the SVG`)
+    } else if (
+      underlayYPositionBottom > overlay[0] &&
+      underlayYPositionTop <= overlay[0]
+    ) {
+      console.log(`${div.className} is currently within the SVG`)
+    }
+  })
+}
 
 document.addEventListener('scroll', () => {
-  let elemYPositionTop = div1.getBoundingClientRect().top
-  let elemYPositionBottom =
-    div1.getBoundingClientRect().height + div1.getBoundingClientRect().top
-  console.log(elemYPositionTop, elemYPositionBottom)
+  compareElements(svgPosition, divs)
 })
 
 /*
 
-- the check will only fire on scrollY
-- the check will compare the current position of the div within the bounding area and the svgPaths (first item)
+- How do you calculate the opposite of a color in RGB?
+- Example: I have orange (255, 127, 0). How do I calculate the opposite, blue (0, 0, 255)
 
-NEW QUESTION
-- How to compare ranges of numbers against each other
-  Ex: range1 = [100, 200] (100 pixels height)
-      range2 = [800, 1000] (200 pixels height)
+red   - (255, 0, 0)     FF0000
+orange- (255, 127, 0)   FF7F00
+yellow- (255, 255, 0)   FFFF00
+green - (0, 255, 0)     00FF00
+blue  - (0, 0, 255)     0000FF
+purple- (127, 0, 255)   7F00FF
 
-      range 2 will decrease as the page scrolls down
-      if range 2 falls within range 1, then proceed with an action
+white - (0, 0, 0)       FFFFFF
+black - (255, 255, 255) 000000
 
-      possible states (within bounds of range 1)
+- ANSWER:
+- Calculate the inverted color, but invert to either black or white for better contrast  
 
-      range2 = [200, 400] ( range2[0] <= range1[1] || range2[0] >= range1[0] )
-      range2 = [50, 250] (" ") // -------------------------------------------------------------------<<<<<<< CONTINUE FROM HERE
-      range2 = [-100, 100] (" ")
-
-      if (range2[0 or 1] )
 */
