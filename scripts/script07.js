c6b18c1864218fcc92fd45b84e40bf91cd32c8cd
imbrikis <imbrikis@yahoo.com>
2021-04-06 15:36:54 -0600

    styling and base functionality

--- scripts/script07.js
+++ scripts/script07.js
@@ -12,34 +12,41 @@ const cBtnX = document.querySelector('.c-btn-x')
 const dBtnY = document.querySelector('.d-btn-y')
 const dBtnX = document.querySelector('.d-btn-x')
 
-const flipY = (image) => {
-  if (image.style.transform === 'scaleY(-1)') {
-    image.style.transform = 'scaleY(1)'
+const flipArray = {
+  imageA: { x: 1, y: 1 },
+  imageB: { x: 1, y: 1 },
+  imageC: { x: 1, y: 1 },
+  imageD: { x: 1, y: 1 },
+}
+
+for (const item in flipArray) {
+  flipArray[
+    item
+  ].transform = `scale(${flipArray[item].x}, ${flipArray[item].y})`
+}
+
+imageA.style.transform = flipArray.imageA.transform
+imageB.style.transform = flipArray.imageB.transform
+imageC.style.transform = flipArray.imageC.transform
+imageD.style.transform = flipArray.imageD.transform
+
+const flipAxis = (image, data, axis) => {
+  if (data[axis] === -1) {
+    data[axis] = 1
+    data.transform = `scale(${data.x}, ${data.y})`
+    image.style.transform = data.transform
   } else {
-    image.style.transform = 'scaleY(-1)'
+    data[axis] = -1
+    data.transform = `scale(${data.x}, ${data.y})`
+    image.style.transform = data.transform
   }
 }
 
-const flipX = (image) => {
-  if (image.style.transform === 'scaleX(-1)') {
-    image.style.transform = 'scaleX(1)'
-  } else {
-    image.style.transform = 'scaleX(-1)'
-  }
-}
-
-aBtnY.addEventListener('click', () => flipY(imageA))
-
-aBtnX.addEventListener('click', () => flipX(imageA))
-
-bBtnY.addEventListener('click', () => flipY(imageB))
-
-bBtnX.addEventListener('click', () => flipX(imageB))
-
-cBtnY.addEventListener('click', () => flipY(imageC))
-
-cBtnX.addEventListener('click', () => flipX(imageC))
-
-dBtnY.addEventListener('click', () => flipY(imageD))
-
-dBtnX.addEventListener('click', () => flipX(imageD))
+aBtnY.addEventListener('click', () => flipAxis(imageA, flipArray.imageA, 'y'))
+aBtnX.addEventListener('click', () => flipAxis(imageA, flipArray.imageA, 'x'))
+bBtnY.addEventListener('click', () => flipAxis(imageB, flipArray.imageB, 'y'))
+bBtnX.addEventListener('click', () => flipAxis(imageB, flipArray.imageB, 'x'))
+cBtnY.addEventListener('click', () => flipAxis(imageC, flipArray.imageC, 'y'))
+cBtnX.addEventListener('click', () => flipAxis(imageC, flipArray.imageC, 'x'))
+dBtnY.addEventListener('click', () => flipAxis(imageD, flipArray.imageD, 'y'))
+dBtnX.addEventListener('click', () => flipAxis(imageD, flipArray.imageD, 'x'))
