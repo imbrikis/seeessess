e95fd77f326446f43355090b7e34c9bab5fe19cb
imbrikis <imbrikis@yahoo.com>
2021-04-14 10:40:19 -0600

    observer now observers... observingly

--- scripts/script08.js
+++ scripts/script08.js
@@ -1,4 +1,27 @@
-const elem = document.querySelector('.all-the-boxes')
-elem.addEventListener('click', () => {
-  console.log(elem)
+const wrapper = document.querySelector('.wrapper')
+const elems = document.querySelector('.all-the-boxes')
+console.log(elems)
+
+let elemsNew = [elems, elems]
+console.log(elemsNew)
+
+elems.addEventListener('click', () => {
+  console.log('clicked')
+  wrapper.append(...elemsNew)
 })
+
+let options = {
+  root: wrapper,
+  rootMargin: '0px',
+  threshold: [0.25, 0.75],
+}
+
+const callback = (entries, observer) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+  })
+}
+
+let observer = new IntersectionObserver(callback, options)
+
+// observer.observe(elems)
