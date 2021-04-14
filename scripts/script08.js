84c232c27ca8c73abb13af19b63b8b9f25a71589
imbrikis <imbrikis@yahoo.com>
2021-04-14 12:45:46 -0600

    infinite scroll on right drag works!

--- scripts/script08.js
+++ scripts/script08.js
@@ -1,14 +1,18 @@
 const wrapper = document.querySelector('.wrapper')
-const elems = document.querySelector('.all-the-boxes')
-console.log(elems)
+let elems = document.querySelectorAll('.box-container')
+const singleBoxContainer = document.querySelector('.box-container')
 
-let elemsNew = [elems, elems]
-console.log(elemsNew)
+window.onload = () => {
+  wrapper.scroll(singleBoxContainer.clientWidth, 0)
+}
 
-elems.addEventListener('click', () => {
-  console.log('clicked')
-  wrapper.append(...elemsNew)
-})
+const clone = singleBoxContainer.cloneNode(true)
+
+// elems.addEventListener('click', () => {
+//   console.log('clicked')
+//   wrapper.appendChild(clone)
+//   wrapper.prepend(clone)
+// })
 
 let options = {
   root: wrapper,
@@ -19,9 +23,38 @@ let options = {
 const callback = (entries, observer) => {
   entries.forEach((entry) => {
     console.log(entry)
+    if (!entry.isIntersecting && entry.boundingClientRect.left > 600) {
+      console.log('move the group at elem[2] to the beginning of the array')
+      fuckery(elems[2])
+    }
+    // attachObservers()
+    // if (entry.isIntersecting && entry.boundingClientRect.left > 600) {
+    //   console.log('move the group at elem[0] to the beginning of the array')
+    // }
+    // if (!entry.isIntersecting && entry.boundingClientRect < 600) {
+    //   console.log('move the group at elem[0] to the end of the array')
+    // }
+    // if (entry.isIntersecting && entry.boundingClientRect < 600) {
+    //   console.log('move the group at elem[2] to the end of the array')
+    // }
   })
 }
 
 let observer = new IntersectionObserver(callback, options)
 
-// observer.observe(elems)
+const attachObservers = () => {
+  observer.observe(elems[1])
+  setInterval(() => console.log(wrapper.scrollLeft), 250)
+}
+
+attachObservers()
+
+const fuckery = (element) => {
+  const oldScrollWidth = wrapper.scrollWidth
+  wrapper.prepend(element)
+  wrapper.scrollLeft = oldScrollWidth - singleBoxContainer.clientWidth * 1.25
+
+  elems = document.querySelectorAll('.box-container')
+  observer.unobserve(elems[1])
+  observer.observe(elems[1])
+}
