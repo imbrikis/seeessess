ed4caeb48fb921a372c7eea2500b27b102520afb
imbrikis <imbrikis@yahoo.com>
2021-04-21 22:35:48 -0600

    now the background shifts color... whoa

--- scripts/script09.js
+++ scripts/script09.js
@@ -1,7 +1,9 @@
 const box = document.querySelector('.box')
+const container = document.querySelector('.container')
 
 box.addEventListener('click', () => {
   box.classList.toggle('moveUp')
+  container.classList.toggle('colorShift')
 })
 
 console.log(box.style)
