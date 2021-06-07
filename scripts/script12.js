1adc76d8dc33219a9f80e09ef30f25d9450f8e07
imbrikis <imbrikis@yahoo.com>
2021-06-06 23:29:27 -0600

    project 12 - added to template

--- scripts/script12.js
+++ scripts/script12.js
@@ -1,6 +1,33 @@
-const canvas = document.querySelector('.container')
+const canvas = document.querySelector('.canvas')
 const ctx = canvas.getContext('2d')
 
-ctx.moveTo(0, 0)
-ctx.lineTo(500, 500)
-ctx.stroke()
+let widthP, heightP
+
+// draw stuff here
+const draw = () => {
+  ctx.moveTo(0, 0)
+  ctx.lineTo(widthP * 1000, heightP * 1000)
+  ctx.stroke()
+}
+
+const resizeCanvas = () => {
+  if (
+    window.innerHeight > window.innerWidth ||
+    window.innerHeight === window.innerWidth
+  ) {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerWidth
+  } else {
+    canvas.width = window.innerHeight
+    canvas.height = window.innerHeight
+  }
+
+  widthP = canvas.width / 1000
+  heightP = canvas.height / 1000
+
+  draw()
+}
+
+window.addEventListener('resize', resizeCanvas)
+
+resizeCanvas()
