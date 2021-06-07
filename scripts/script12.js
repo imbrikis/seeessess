5612a4bc39d6851ba90e2161cf961842d6e24a48
imbrikis <imbrikis@yahoo.com>
2021-06-06 23:55:41 -0600

    project 12 - added anim frames

--- scripts/script12.js
+++ scripts/script12.js
@@ -1,16 +1,31 @@
 const canvas = document.querySelector('.canvas')
 const ctx = canvas.getContext('2d')
 
-let widthP, heightP
+let widthP, heightP, animFrame
 
 // draw stuff here
 const draw = () => {
-  ctx.moveTo(0, 0)
-  ctx.lineTo(widthP * 1000, heightP * 1000)
-  ctx.stroke()
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.05)'
+  ctx.fillRect(0, 0, widthP * 1000, heightP * 1000)
+  ctx.fillStyle = 'rgba(255, 255, 255, 1)'
+
+  const radius = Math.floor(Math.random() * 100) + 1
+
+  ctx.beginPath()
+  ctx.arc(
+    Math.floor(Math.random() * (widthP * 1000) + 1),
+    Math.floor(Math.random() * (widthP * 1000) + 1),
+    radius,
+    0,
+    Math.PI * 2
+  )
+  ctx.fill()
+  console.log('hi')
+  animFrame = window.requestAnimationFrame(draw)
 }
 
 const resizeCanvas = () => {
+  window.cancelAnimationFrame(animFrame)
   if (
     window.innerHeight > window.innerWidth ||
     window.innerHeight === window.innerWidth
@@ -25,7 +40,7 @@ const resizeCanvas = () => {
   widthP = canvas.width / 1000
   heightP = canvas.height / 1000
 
-  draw()
+  animFrame = window.requestAnimationFrame(draw)
 }
 
 window.addEventListener('resize', resizeCanvas)
