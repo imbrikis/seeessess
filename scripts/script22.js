0b8cbb1a976647555b4314a07bed2779f1e7374e
imbrikis <imbrikis@yahoo.com>
2021-08-12 16:41:47 -0600

    page 22 - IT WORKS!!!

--- scripts/script22.js
+++ scripts/script22.js
@@ -5,9 +5,8 @@ const file3 = document.querySelector('.file3')
 const file4 = document.querySelector('.file4')
 const file5 = document.querySelector('.file5')
 
-let windowPositions = [1, 2, 3, 4, 5]
-let selectedWindowIndex = windowPositions.indexOf(5)
-console.log(selectedWindowIndex)
+let windowPositions = [0, 1, 2, 3, 4]
+let selectedWindowIndex = windowPositions.indexOf(4)
 
 const initFiles = () => {
   for (let x = 0; x < allFiles.length; x++) {
@@ -17,28 +16,23 @@ const initFiles = () => {
 initFiles()
 
 const windowFocus = (e) => {
-  console.log(selectedWindowIndex)
-  if (e.target.style.zIndex - 1 === selectedWindowIndex) {
+  if (+e.target.style.zIndex === 4) {
     console.log("i won't do it")
     return
   }
 
   selectedWindowIndex = +e.target.style.zIndex
 
-  console.log(selectedWindowIndex)
+  for (let x = 0; x < allFiles.length; x++) {
+    if (+allFiles[x].style.zIndex === selectedWindowIndex) {
+      windowPositions[x] = 4
+    }
+    if (+allFiles[x].style.zIndex > selectedWindowIndex) {
+      windowPositions[x] = windowPositions[x] - 1
+    }
+  }
 
-  // for (let x = 0; x < allFiles.length; x++) {
-  //   console.log(selectedWindowIndex)
-  //   if (allFiles[x].style.zIndex - 1 === selectedWindowIndex) {
-  //     windowPositions[x] = x + 1
-  //   }
-  //   if (allFiles[x].style.zIndex - 1 > selectedWindowIndex) {
-  //     windowPositions[x] = windowPositions[x] - 1
-  //   }
-  // }
-  // console.log(selectedWindowIndex)
-
-  // initFiles()
+  initFiles()
 }
 
 file1.addEventListener('click', windowFocus)
