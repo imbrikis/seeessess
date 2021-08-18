1985fff3537b8525ee5e19384049f269845352d0
imbrikis <imbrikis@yahoo.com>
2021-08-17 23:01:10 -0600

    page 22 progress

--- scripts/script22.js
+++ scripts/script22.js
@@ -6,6 +6,10 @@ const file4 = document.querySelector('.file4')
 const file5 = document.querySelector('.file5')
 
 let active
+let startX = 0
+let startY = 0
+let tempX = 0
+let tempY = 0
 
 let windowPositions = [0, 1, 2, 3, 4]
 let selectedWindowIndex = windowPositions.indexOf(4)
@@ -38,21 +42,37 @@ const windowFocus = (e) => {
 }
 
 const dragStart = (e, file) => {
-  console.log(e)
+  console.log(e, file.style)
   active = true
+  startX = file.style.top - 'px' || 0
+  startY = file.style.left - 'px' || 0
 
-  // file.style.top = e.clientY + 'px'
-  // file.style.left = e.clientX + 'px'
+  console.log(startX)
 }
 
-const dragEnd = (e) => {
-  console.log(e)
+const dragEnd = (e, file) => {
+  console.log(e, file)
   active = false
+  tempX = +(file.style.top - 'px') || 0
+  tempY = +(file.style.left - 'px') || 0
+  startX = +(file.style.top - 'px') || 0
+  startY = +(file.style.left - 'px') || 0
 }
 
-const drag = (e) => {
+const drag = (e, file) => {
   if (active) {
-    console.log(e)
+    console.log(e, file)
+
+    tempX = +(e.clientX - startX)
+    tempY = +(e.clientY - startY)
+
+    file.style.top = tempY + 'px'
+    file.style.left = tempX + 'px'
+
+    // console.log(file.style.top)
+    // console.log(file.style.left)
+    // console.log(tempX)
+    // console.log(tempY)
   }
 }
 
